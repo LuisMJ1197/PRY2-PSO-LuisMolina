b12431d02dc256fa38a32d3dfff13ef29fbdb66f
LuisMJ1197 <luisfermjua@gmail.com>
2020-11-23 01:00:36 -0600

    FCFS casi completo. Ejecución corriendo bien, creo.

--- PRY2-PSO-LuisMolina/src/os/processmanagement/FCFSScheduler.java
+++ PRY2-PSO-LuisMolina/src/os/processmanagement/FCFSScheduler.java
@@ -5,10 +5,32 @@
  */
 package os.processmanagement;
 
+import machine.processor.Core;
+import os.PCB;
+import os.process.Process;
+
 /**
  *
  * @author Luism
  */
-public class FCFSScheduler implements IScheduler {
+public class FCFSScheduler extends Scheduler {
+
+    public FCFSScheduler() {
+        
+    }
+    
+    @Override
+    public Process chooseNextTurn() {
+        Process next = this.processQueue.dequeue();
+        if (next == null) return null;
+        this.processor.getCores()[next.getPcb().getCpuNumber() - 1]
+                .setBurstTime(next.getPcb().getLimit());
+        return next;
+    }
+
+    @Override
+    public void setNextBurstTime(Core core, Process process) {
+        core.setBurstTime(process.getBurstTime());
+    }
     
 }
